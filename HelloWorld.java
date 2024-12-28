f5791ee61f94ba9b303e2757adc1598ecc3a71e9
Sai0045 <sairajabhale@gmail.com>
2024-12-28 18:06:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!,Hii its Sai");
+        System.out.println("Hello, World!,Sai");
     }
 }
